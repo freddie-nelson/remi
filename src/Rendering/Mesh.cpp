c970a8d2d69de29f7125d80ce9f2bd5cf7112ecd
Freddie Nelson <47699795+freddie-nelson@users.noreply.github.com>
2023-11-03 22:54:13 +0000

    chore: find convex vertices function

--- src/Rendering/Mesh.cpp
+++ src/Rendering/Mesh.cpp
@@ -1,10 +1,21 @@
 #include "../../include/Rendering/Mesh.h"
 
+#include <stdexcept>
+#include <list>
+#include <glm/glm.hpp>
+#include <numbers>
+
 Rendering::Mesh2D Rendering::triangulate(const std::vector<glm::vec2> &vertices)
 {
+    if (vertices.size() < 3)
+    {
+        throw std::invalid_argument("Cannot triangulate a polygon with less than 3 vertices.");
+    }
+
     Mesh2D mesh;
 
-    if (vertices.size() < 3)
+    // if the polygon is a triangle, just return it
+    if (vertices.size() == 3)
     {
         for (int i = 0; i < vertices.size(); i++)
         {
@@ -16,4 +27,50 @@ Rendering::Mesh2D Rendering::triangulate(const std::vector<glm::vec2> &vertices)
     }
 
     // triangulate using ear clipping
+
+    // copy vertices into doubly linked list
+    std::list<glm::vec2> verticesList;
+    int numVertices = vertices.size();
+
+    for (auto &vertex : vertices)
+    {
+        verticesList.push_back(vertex);
+    }
+}
+
+/**
+ * Finds the convex vertices of the given vertices.
+ *
+ * A vertex is convex if the it creates a convex angle with its neighbours.
+ *
+ * A convex angle is an angle that is less than 180 degrees.
+ *
+ * @param vertices The vertices to find the convex vertices of.
+ * @return The convex vertices of the given vertices.
+ */
+std::vector<glm::vec2> findConvexVertices(const std::vector<glm::vec2> &vertices)
+{
+    std::vector<glm::vec2> convexVertices;
+
+    for (int i = 0; i < vertices.size(); i++)
+    {
+        auto v = vertices[i];
+        auto prev = vertices[(i - 1) % vertices.size()];
+        auto next = vertices[(i + 1) % vertices.size()];
+
+        // get angle between previous and next vertices
+        // formula explained here: https://byjus.com/maths/angle-between-two-lines/
+        float m1 = (v.y - prev.y) / (v.x - prev.x);
+        float m2 = (next.y - v.y) / (next.x - v.x);
+
+        float angle = std::atan((m2 - m1) / (1 + m1 * m2));
+
+        // if angle is less than 180 degrees, then vertex is convex
+        if (angle < std::numbers::pi)
+        {
+            convexVertices.push_back(v);
+        }
+    }
+
+    return convexVertices;
 }
