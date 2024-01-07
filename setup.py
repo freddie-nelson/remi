7394d464efd4c4d7661156c61b834f746d042191
Freddie Nelson <47699795+freddie-nelson@users.noreply.github.com>
2024-01-07 20:34:21 +0000

    chore: setup script sets up dev too

--- setup.py
+++ setup.py
@@ -17,17 +17,35 @@ args = parser.parse_args()
 target = args.target
 
 # remove build directory
-if os.path.exists(build_path):
-    shutil.rmtree(build_path)
+# if os.path.exists(build_path):
+#     shutil.rmtree(build_path)
 
 # run meson setup
 if target == "native":
-    exit_code = os.system(f"meson setup build")
-else:
+    exit_code = os.system(f"meson setup build --wipe")
+elif target == "wasm":
     cross_file = os.path.join(dir_path, f"cross-{target}.txt")
-    exit_code = os.system(f"meson setup --cross-file {cross_file} build")
+    exit_code = os.system(f"meson setup --wipe --cross-file {cross_file} build")
+else:
+    print("Unknown target!")
+    exit_code = 1
 
 if exit_code != 0:
     print("Failed to setup blaze!")
+    exit(exit_code)
+
+# setup dev
+dev_path = os.path.join(dir_path, "dev")
+
+if target == "native":
+    exit_code = os.system(f"cd {dev_path} && meson setup build --wipe")
+elif target == "wasm":
+    exit_code = os.system(f"cd {dev_path} && meson setup --wipe --cross-file {cross_file} build")
+else:
+    print("Unknown target!")
+    exit_code = 1
+
+if exit_code != 0:
+    print("Failed to setup blaze dev!")
     exit(exit_code)
 
