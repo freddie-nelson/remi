20cf8be8a4525343a9f047516057d19643ca59f6
Freddie Nelson <47699795+freddie-nelson@users.noreply.github.com>
2024-01-08 20:49:14 +0000

    chore: update install script

--- install.py
+++ install.py
@@ -16,28 +16,32 @@ install_lib_path = os.path.join(install_path, "lib")
 install_include_path = os.path.join(install_path, "include")
 install_pkgconfig_file = os.path.join(install_lib_path, "pkgconfig/blaze++.pc")
 
-mingw64_path = "C:/msys64/mingw64"
-mingw64_include_path = os.path.join(mingw64_path, "include")
-mingw64_lib_path = os.path.join(mingw64_path, "lib")
-mingw64_pkgconfig_path = os.path.join(mingw64_path, "lib/pkgconfig")
+# msys64_envs = ["mingw64", "ucrt64"]
+msys64_envs = ["mingw64"]
 
-if os.path.exists(mingw64_path):
-    # copy headers
-    shutil.copytree(install_include_path, mingw64_include_path, dirs_exist_ok=True)
+for env in msys64_envs:
+    path = f"C:/msys64/{env}"
+    include_path = os.path.join(path, "include")
+    lib_path = os.path.join(path, "lib")
+    pkgconfig_path = os.path.join(path, "lib/pkgconfig")
 
-    # copy libs
-    for file in os.listdir(install_lib_path):
-        if file == "pkgconfig":
-            continue
+    if os.path.exists(path):
+        # copy headers
+        shutil.copytree(install_include_path, include_path, dirs_exist_ok=True)
+
+        # copy libs
+        for file in os.listdir(install_lib_path):
+            if file == "pkgconfig":
+                continue
 
 
-        path = os.path.join(install_lib_path, file)
-        new_path = os.path.join(mingw64_lib_path, file)
+            path = os.path.join(install_lib_path, file)
+            new_path = os.path.join(lib_path, file)
 
-        if os.path.exists(new_path):
-            os.remove(new_path)
+            if os.path.exists(new_path):
+                os.remove(new_path)
 
-        shutil.copy(path, new_path)
+            shutil.copy(path, new_path)
 
-    # copy pkgconfig
-    shutil.copy(install_pkgconfig_file, mingw64_pkgconfig_path)
+        # copy pkgconfig
+        shutil.copy(install_pkgconfig_file, pkgconfig_path)
