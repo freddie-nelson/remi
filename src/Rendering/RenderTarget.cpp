75cb54788f86ba6e4a64aa0cc2ca9ad795eab2f4
Freddie Nelson <47699795+freddie-nelson@users.noreply.github.com>
2024-02-18 18:36:58 +0000

    fix: render target texture wrap mode wrong

--- src/Rendering/RenderTarget.cpp
+++ src/Rendering/RenderTarget.cpp
@@ -220,6 +220,8 @@ GLuint Rendering::RenderTarget::createTexture() {
     // TODO: make filter mode configurable?
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
     // unbind texture
     glBindTexture(GL_TEXTURE_2D, 0);
