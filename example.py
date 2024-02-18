82aefcd8b34475f5d4c64a664f2a2afd13af3050
Freddie Nelson <47699795+freddie-nelson@users.noreply.github.com>
2024-02-18 20:39:00 +0000

    fix: example wasm build

--- example.py
+++ example.py
@@ -5,6 +5,8 @@ import argparse
 import subprocess
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
+wasm_cross_file = os.path.join(base_dir, 'cross/wasm.txt')
+
 examples_dir = os.path.join(base_dir, 'examples')
 
 examples_build_dir_name = '__build'
@@ -34,8 +36,15 @@ def read_example(example_dir):
     
     return info
 
-def build_example(example, clean=True):
+def build_example(example, clean=True, target="native", mode="debug"):
     build_dir = os.path.join(example['dir'], examples_build_dir_name)
+
+    if target == "wasm":
+        if not os.path.exists(wasm_cross_file):
+            raise Exception(f"Cannot find cross file for wasm at {wasm_cross_file}.")
+        
+        if not os.path.exists(os.path.join(base_dir, "lib/lib/libremi.a")):
+            raise Exception(f"Cannot find libremi.a in {base_dir}. Please build remi for wasm first.")
 
     if os.path.exists(build_dir) and clean:
         shutil.rmtree(build_dir)
@@ -45,7 +54,7 @@ def build_example(example, clean=True):
         os.makedirs(build_dir)
         
         # copy the example directory to the build directory
-        shutil.copytree(example['dir'], build_dir, dirs_exist_ok=True, ignore=shutil.ignore_patterns("info.json", examples_build_dir_name, example['assets_dir']))
+        shutil.copytree(example['dir'], build_dir, dirs_exist_ok=True, ignore=shutil.ignore_patterns("info.json", examples_build_dir_name))
 
         # copy example build files to build dir
         shutil.copytree(examples_build_dir, build_dir, dirs_exist_ok=True)
@@ -66,7 +75,11 @@ def build_example(example, clean=True):
 
     # setup the example build if needed
     if not os.path.exists(os.path.join(build_dir, 'build')):
-        exit_code, output = subprocess.getstatusoutput(f"cd {build_dir} && meson setup build")
+        if target == "wasm":
+            exit_code, output = subprocess.getstatusoutput(f"cd {build_dir} && meson setup build --cross-file {wasm_cross_file} --buildtype {mode}")
+        else:
+            exit_code, output = subprocess.getstatusoutput(f"cd {build_dir} && meson setup build --buildtype {mode}")
+        
 
         if exit_code != 0:
             raise Exception(f"Failed to setup the example build: {output}")
@@ -84,11 +97,12 @@ def build_example(example, clean=True):
         shutil.copytree(os.path.join(example['dir'], example['assets_dir']), os.path.join(build_dir, 'build', example['assets_dir']), dirs_exist_ok=True)
     
     # copy in remi dll
-    remi_dll = os.path.join(base_dir, "lib/lib/libremi.dll")
-    if not os.path.exists(remi_dll):
-        raise Exception(f"Cannot find remi.dll in {remi_dll}.")
-    
-    shutil.copy(remi_dll, os.path.join(build_dir, 'build'))
+    if target == "native":
+        remi_dll = os.path.join(base_dir, "lib/lib/libremi.dll")
+        if not os.path.exists(remi_dll):
+            raise Exception(f"Cannot find remi.dll in {remi_dll}.")
+        
+        shutil.copy(remi_dll, os.path.join(build_dir, 'build'))
 
     return build_dir
 
@@ -106,7 +120,7 @@ def run_example(example):
             
         os.rename(os.path.join(meson_build_dir, "example.html"), os.path.join(meson_build_dir, "example.html"))
 
-        os.system(f"start http://localhost:8000")
+        os.system(f"start http://localhost:8000/example.html")
         os.system(f"cd {meson_build_dir} && python -m http.server 8000")
     elif os.path.exists(os.path.join(meson_build_dir, "example.exe")):
         os.system(f"cd {meson_build_dir} && example.exe")
@@ -114,11 +128,11 @@ def run_example(example):
         os.system(f"cd {meson_build_dir} && example")
 
 
-def run_and_build_example(example, clean=True):
+def run_and_build_example(example, clean=True, target="native", mode="debug"):
     print(f"{example['name']}: {example['description']}")
 
     print("Building...")
-    build_example(example, clean)
+    build_example(example, clean, target, mode)
 
     print("Running...")
     run_example(example)
@@ -127,7 +141,15 @@ def run_and_build_example(example, clean=True):
 # parse arguments
 parser = argparse.ArgumentParser("example")
 parser.add_argument("example", help="Choose the example to build and run", choices=get_examples_names())
+parser.add_argument("--clean", help="Clean the build directory before building the example", choices=["true", "false"], default="true")
+parser.add_argument("--target", help="The target to build the example for", choices=["wasm", "native"], default="native")
+parser.add_argument("--mode", help="The build mode", choices=["debug", "release"], default="release")
 
 args = parser.parse_args()
 
-run_and_build_example(read_example(os.path.join(examples_dir, args.example)))
+example = read_example(os.path.join(examples_dir, args.example))
+clean = args.clean == "true"
+target = args.target
+mode = args.mode
+
+run_and_build_example(example, clean, target, mode)
